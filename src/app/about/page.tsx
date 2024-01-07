a0a92c7c2912f4d7dda54e917dccf8aff3c74db6
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2024-01-07 11:27:50 -0600

    added an about page to practice how to use the App router

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function About() {
+    return (
+        <>
+            <div>
+                <Link href="/">Home</Link>
+                <h1>About</h1>
+                <h2>This pages tells you about the project.</h2>
+            </div>
+
+        </>
+    )
+}
